8c9d60ca292e9e5cb7cf265b7b83444b6b74e4fa
Dylan D <domy7912@gmail.com>
2023-09-07 22:20:19 -0400

    Binary Search updated with template code

--- 704_Binary_Search.py
+++ 704_Binary_Search.py
@@ -1,17 +1,19 @@
-# Working code
+# My updated solution using the code template
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # We have an assortded array so we can use binary search
 
         l, r = 0, len(nums) - 1
 
-        while l <= r:
+        while l < r:
             m = l + (r - l) // 2
-            if nums[m] == target:
-                return m
-            elif nums[m] > target:
-                r = m - 1
-            elif nums[m] < target:
+            if target <= nums[m]:
+                r = m
+            else:
                 l = m + 1
+
+        if nums[l] == target:
+            return l
 
         return -1
